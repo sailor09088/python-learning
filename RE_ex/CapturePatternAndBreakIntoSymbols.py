8ac8dfd27ce13ee9a1b342ffb17c6c2696d4482d
sailor09088 <qi_peijun@hotmail.com>
2019-10-09 16:18:52 +0800

    Rev1.0_20191009
    Working version

---
+++ RE_ex/CapturePatternAndBreakIntoSymbols.py
@@ -0,0 +1,5 @@
+#Python3 exercise code
+#Capture lines with special pattern
+#Break lines into symbols
+#Print symbols in pre-determined order
+#
--- RE_ex/SearchAndReplacement.py
+++ RE_ex/SearchAndReplacement.py
@@ -1,5 +1,36 @@
-#Exercise code
-#Search and replacement with R.E.
+#Rev 0.1
+#Python3 exercise code
+#Search and replace unicode strings with R.E.
 #Recursive or limited iterations
 #Support wildcard symbol
 #Print warnings if number of augment is less or more
+#Break any line to strings
+
+#!/usr/bin/env python3
+
+import re
+import sys
+
+#list of pattern followed by substitute
+list_pattern_sub_pair = [
+	'PMOD1', 'PMOD2',
+	'CDM', 'XDM',
+	'DMA', 'DMB',
+	'DMC', 'DMD'
+	]
+list_length = len(list_pattern_sub_pair)
+
+#print (list_length)
+
+n=0
+
+file = open(sys.argv[1])
+for line in file:
+	while n < list_length:
+		line = re.sub(list_pattern_sub_pair[n],list_pattern_sub_pair[n+1], line)		
+		n = n + 2
+	else:
+		n = 0
+		sys.stdout.write(line.format(**locals()))
+		continue
+#End of file
