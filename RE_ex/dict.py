39ee4320549cf13c10109edec74539f881a071a1
sailor09088 <55517299+sailor09088@users.noreply.github.com>
2023-01-15 09:43:12 +0800

    Add files via upload

--- RE_ex/dict.py
+++ RE_ex/dict.py
@@ -11,9 +11,9 @@ dictionary = {}
 for line in open(sys.argv[1], encoding='utf-16'):
     splt = line.split()
     if len(splt) >= 2:
+#two strings in a line: first as key, and second as value
         dictionary[splt[0]] = splt[1]
-#        print(line)
-#print(dictionary['COME_A100'])
+
 
 for line in open(sys.argv[2]):
     splt = line.split()
@@ -21,9 +21,7 @@ for line in open(sys.argv[2]):
         for i in range (0, len(splt)):
             if splt[i] == 'SIG_NAME' :
                 key = splt[i+1]
-                print(key)
-                gold = dictionary[key]
-                print(gold)
-                line = line.replace(key, gold)
-                print(line)
+                value = dictionary[key]
+                line = line.replace(key, value)
+    print(line)
             
